587fcf2687e1602b66f3fc1f76aa8002eefeb2a7
dulmi1228 <48253729+dulmi1228@users.noreply.github.com>
2019-04-05 09:29:37 +0530

    JavaProgramme

---
+++ src/java2/Java2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java2;
+
+/**
+ *
+ * @author Dulmi
+ */
+public class Java2 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+               int num=8;
+       if (num>10){
+           System.out.println("Greater than 10");
+       }
+       else{
+       System.out.println("Less than 10");
+
+    }
+    }
+    }
